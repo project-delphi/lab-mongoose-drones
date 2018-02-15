c594aaef20b4b3a756f8257ebc21b9b8eb576102
Ravi Kalia <ravkalia@gmail.com>
2018-02-15 13:59:24 -0600

    drone views

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,21 +1,27 @@
-const express = require('express');
+const express = require('express')
 
 // require the Drone model here
 
-const router = express.Router();
-
+const router = express.Router()
+const Drone  = require('../models/drone')
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2
-});
+  Drone.find({}, (err,drones) => {
+
+  })
+  res.render('drones/index', {
+    drones: drones
+  })
+})
 
 
 router.get('/drones/new', (req, res, next) => {
   // Iteration #3
-});
+})
 
 router.post('/drones', (req, res, next) => {
   // Iteration #3
-});
+})
 
-module.exports = router;
+module.exports = router
