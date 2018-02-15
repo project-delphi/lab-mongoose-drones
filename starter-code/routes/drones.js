03b2abd56c372b77e5a997901171365b726af390
Ravi Kalia <ravkalia@gmail.com>
2018-02-15 16:09:42 -0600

    iteration 3 almost done

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -7,7 +7,7 @@ const Drone  = require('../models/drone')
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2
-  Drone.find({}, (err,drones) => {
+  Drone.find({}, (err, drones) => {
 
   })
   res.render('drones/index', {
@@ -17,11 +17,25 @@ router.get('/drones', (req, res, next) => {
 
 
 router.get('/drones/new', (req, res, next) => {
-  // Iteration #3
+// Iteration 3
+  res.render('/drones/new')
 })
 
 router.post('/drones', (req, res, next) => {
-  // Iteration #3
+// Iteration 3
+  const drone_data = {
+    name : req.body.name,
+    propellers : req.body.propellers,
+    maxSpeed : req.body.maxSpeed
+  }
+    drone = new Drone(drone_data)
+    drone.save( , (err, data) => {
+      if(err){
+      next()
+      } else {
+        router.get('/drones')
+      }
+  })
 })
 
 module.exports = router
